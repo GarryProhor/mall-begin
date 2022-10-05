392868cebbbfe444910fa39983b38574c7f675a1
GarryProhor <igorprohorchenko@gmail.com>
2022-10-05 15:45:36 +0300

    sticky

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './Header.css';
 import {Container, Row} from "reactstrap";
 import logo from '../../assets/images/eco-logo.png';
@@ -8,8 +8,24 @@ import {navLink} from "../../assets/data/navLink";
 import {motion} from "framer-motion";
 
 const Header = () => {
+    const headerRef  = useRef(null);
+    const stickyHeaderFunc = () =>{
+        window.addEventListener('scroll', ()=>{
+            if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+                headerRef.current.classList.add('sticky__header');
+            }else {
+                headerRef.current.classList.remove('sticky__header');
+            }
+        })
+    }
+    useEffect(() => {
+        stickyHeaderFunc();
+
+        return () => window.removeEventListener('scroll', stickyHeaderFunc);
+    });
+
     return (
-        <header className='header'>
+        <header className='header' ref={headerRef}>
             <Container>
                 <Row>
                     <div className='nav__wrapper'>
