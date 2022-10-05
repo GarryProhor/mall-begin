12b54e7293ffebe532087d29cda479923b337296
GarryProhor <igorprohorchenko@gmail.com>
2022-10-05 14:39:29 +0300

    timer section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,6 +8,8 @@ import {motion} from "framer-motion";
 import Services from "../../components/UI/services";
 import Products from "../../components/UI/Products";
 import products from "../../assets/data/products";
+import counterImg from '../../assets/images/counter-timer-img.png';
+import Clock from "../../components/UI/CLock";
 
 
 const Home = () => {
@@ -66,6 +68,25 @@ const Home = () => {
                             <h2 className='section__title'>Best Sales</h2>
                         </Col>
                         <Products data={bestSalesProducts}/>
+                    </Row>
+                </Container>
+            </section>
+            <section className='timer__count'>
+                <Container>
+                    <Row>
+                        <Col lg='6' md='6'>
+                            <div className='clock__top-content'>
+                                <h4 className='text-white fs-6 mb-2'>Limited Offers</h4>
+                                <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
+                            </div>
+                            <Clock />
+                            <motion.button whileHover={{scale: 1.2}} className='buy__btn store__btn'>
+                                <Link to='/shop'>Visit Store</Link>
+                            </motion.button>
+                        </Col>
+                        <Col lg='6' md='6' className='text-end'>
+                            <img src={counterImg} alt='counter-img'/>
+                        </Col>
                     </Row>
                 </Container>
             </section>
