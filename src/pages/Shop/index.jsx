d3aeede2cb48f70ce1908da182bb0ed066074c7c
GarryProhor <igorprohorchenko@gmail.com>
2022-10-05 21:32:35 +0300

    filtered by category

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,12 +1,99 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Shop.css';
 import Helmet from "../../components/Helmet";
 import CommonSection from "../../components/UI/CommonSection";
+import {Col, Container, Row} from "reactstrap";
+import {useSelector} from "react-redux";
+import products from "../../assets/data/products";
+import Products from "../../components/UI/Products";
 
 const Shop = () => {
+    const [productsData, setProductsData] = useState(products);
+
+    const handleFilter = (e) => {
+        const filterValue = e.target.value;
+        if(filterValue === 'sofa'){
+            const filteredProducts = products.filter(
+                (item) => item.category === 'sofa'
+            )
+            setProductsData(filteredProducts);
+        }
+        if(filterValue === 'mobile'){
+            const filteredProducts = products.filter(
+                (item) => item.category === 'mobile'
+            )
+            setProductsData(filteredProducts);
+        }
+        if(filterValue === 'chair'){
+            const filteredProducts = products.filter(
+                (item) => item.category === 'chair'
+            )
+            setProductsData(filteredProducts);
+        }
+        if(filterValue === 'watch'){
+            const filteredProducts = products.filter(
+                (item) => item.category === 'watch'
+            )
+            setProductsData(filteredProducts);
+        }
+        if(filterValue === 'wireless'){
+            const filteredProducts = products.filter(
+                (item) => item.category === 'wireless'
+            )
+            setProductsData(filteredProducts);
+        }
+    };
+
     return (
         <Helmet title='Shop'>
             <CommonSection title='Products' />
+
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='3' md='3'>
+                            <div className='filter__widget'>
+                                <select onChange={handleFilter}>
+                                    <option>Filter By Category</option>
+                                    <option value='sofa'>Sofa</option>
+                                    <option value='mobile'>Mobile</option>
+                                    <option value='chair'>Chair</option>
+                                    <option value='watch'>Watch</option>
+                                    <option value='wireless'>Wireless</option>
+                                </select>
+                            </div>
+                        </Col>
+                        <Col lg='3' md='3'>
+                            <div className='filter__widget'>
+                                <select>
+                                    <option>Sort By</option>
+                                    <option value='ascending'>Ascending</option>
+                                    <option value='descending'>Descending</option>
+                                </select>
+                            </div>
+                        </Col>
+                        <Col lg='6' md='6'>
+                            <div className='search__box'>
+                                <input type='text' placeholder='Search...'/>
+                                <span><i className="ri-search-line"></i></span>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            <section className='pt-0'>
+                <Container>
+                    <Row>
+                        {
+                            productsData.length === 0 ? (
+                                <h1>No products are found!</h1>
+                            ) : (
+                                <Products data={productsData} />
+                            )
+                        }
+                    </Row>
+                </Container>
+            </section>
         </Helmet>
     );
 };
