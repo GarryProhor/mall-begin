989c55e5bf294529ddef0cf2b467c10675c36ab5
GarryProhor <igorprohorchenko@gmail.com>
2022-10-05 15:33:22 +0300

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,91 @@
 import React from 'react';
 import './Footer.css';
+import {Col, Container, ListGroup, ListGroupItem, Row} from "reactstrap";
+import logo from "../../assets/images/eco-logo.png";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='footer'>
+            <Container>
+                <Row>
+                    <Col lg='4'>
+                        <div className='logo'>
+                            <div>
+                                <h1 className='text-white'>ProhorMall</h1>
+                                {/*<p>Since 1986</p>*/}
+                            </div>
+                        </div>
+                        <p className='footer__text'>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur dolore, dolorem expedita quo tenetur voluptatum.
+                        </p>
+                    </Col>
+                    <Col lg='3'>
+                        <div className='footer__quick-links'>
+                            <h4 className='quick__links-title'>Top Categories</h4>
+                            <ListGroup className='mb-3'>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='#'>Mobile Phone</Link>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='#'>Modern Sofa</Link>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='#'>Arm Chair</Link>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='#'>Smart Watches</Link>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </div>
+                    </Col>
+                    <Col lg='2'>
+                        <div className='footer__quick-links'>
+                            <h4 className='quick__links-title'>Useful Links</h4>
+                            <ListGroup className='mb-3'>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='/shop'>Shop</Link>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='/cart'>Cart</Link>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='/login'>Login</Link>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0'>
+                                    <Link to='#'>Privacy Policy</Link>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </div>
+                    </Col>
+                    <Col lg='3'>
+                        <div className='footer__quick-links'>
+                            <h4 className='quick__links-title'>Contact</h4>
+                            <ListGroup className='footer__contact mb-3'>
+                                <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-2'>
+                                    <span><i className='ri-map-pin-line'></i></span>
+                                    <p>246050, Homel, Belarus</p>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-2'>
+                                    <span><i className='ri-phone-line'></i></span>
+                                    <p>+375296666666</p>
+                                </ListGroupItem>
+                                <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-2'>
+                                    <span><i className='ri-mail-line'></i></span>
+                                    <p>garryprohor@gmail.com</p>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </div>
+                    </Col>
+                    <Col lg='12'>
+                        <p className='footer__copyright'>
+                            Copyright {year} developed by Garry Prohor. All rights reserved.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     );
 };
 
